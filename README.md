48a6922d216b426fb5c4ed165c4a8036997a7974
diop <fodesdiop@gmail.com>
2019-04-29 13:00:56 -0700

    reset repo

---
+++ README.md
@@ -0,0 +1,47 @@
+# Gëstu
+Learn Android Programming in Wolof
+
+## App
+This web app powers [https://www.gestu.co](https://www.gestu.co)
+
+## Rationale
+Study Computer Science while working on the most exciting technology of the century. Courses are also offered in multiple natural languages: Wolof, French and English.
+
+## Getting started
++ Install yarn if you don't already have it on your machine.
+
+```
+$ brew update
+$ brew install yarn
+```
+
++ Install GatsbyJS
+
+```
+$ npm install -g gatsby-cli
+```
+
++ Clone and navigate to this repo, then:
+
+```
+$ yarn
+$ gatsby develop 
+```
+
++ Web endpoint
+
+``` http://localhost:8000 ```
+
++ GraphQL endpoint
+
+``` http://localhost:8000/___graphql ```
+
+
+## Technology stack
++ GatsbyJS: [https://www.gatsbyjs.org/](https://www.gatsbyjs.org/)
++ ReactJS: [https://reactjs.org/](https://reactjs.org/)
++ GraphQL: [https://graphql.org/](https://graphql.org/)
++ Netlify: [https://www.netlify.com/](https://www.netlify.com/)
++ Contentful: [https://www.contentful.com/developers/docs/](https://www.contentful.com/developers/docs/)
+
+© Copyright 2019 Fodé Diop - MIT License
---
+++ src/components/header.js
@@ -0,0 +1,54 @@
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+import './Header.css'
+import StripeCheckout from 'react-stripe-checkout'
+
+class Header extends React.Component {
+  constructor (props){
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', 
+    this.handleScroll)
+  }
+
+  handleScroll = event => {
+    const scrollTop = window.pageYOffset 
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render () {
+    return (
+      <div className={this.setState.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src={require('../images/gatsby-icon.png')} width="40"></img></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/consulting">Consulting</Link>
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSfm9oGWSqN8gjLx-9KxdHE4OlK2UApbWzygkH-Qm5D7NeU5zQ/viewform"><button>Apply</button></a>
+        </div>  
+      </div>
+    )
+  }
+}
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
